d993e71953e6c1c6c5b09e6f3aa24ee713c8d9b7
Beren Ates <berenatess9918@gmail.com>
2023-10-17 22:46:25 +0300

    Week5Day2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,69 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
+const container = document.getElementsByClassName("main-navigation");
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  console.log("event >", e);
+  container.style.top = e.pageY + "px";
+  container.style.left = e.pageX + "px";
+  container.classList.remove("hide");
+});
+document.addEventListener("click", function (e) {
+  container.classList.add("hide");
+});
 
-// Kodlar buraya gelecek!
+const baslik = document.querySelector(".logo-heading");
+window.addEventListener("click", function (event) {
+  baslik.style.color = "brown";
+});
+
+document.querySelectorAll("nav a").forEach((metin) => {
+  metin.addEventListener("mouseover", (event) => {
+    metin.style.color = "red";
+  });
+});
+
+document.querySelectorAll("nav a").forEach((metin) => {
+  metin.addEventListener("mouseout", (event) => {
+    metin.style.color = "orange";
+  });
+});
+document.querySelectorAll("h2").forEach((metin) => {
+  metin.addEventListener("mouseover", (event) => {
+    metin.style.color = "orange";
+  });
+});
+
+document.querySelectorAll("h2").forEach((metin) => {
+  metin.addEventListener("mouseout", (event) => {
+    metin.style.color = "black";
+  });
+});
+
+window.addEventListener("doubleClick", (e) => {
+  h1.textContent = window.resizeBy;
+});
+
+document.querySelectorAll("img").forEach((photo) => {
+  photo.addEventListener("dblclick", (event) => {
+    photo.style.filter = "blur(5px)";
+  });
+});
+const input = document.createElement("input");
+const footer = document.querySelector(".footer");
+input.type = "text";
+input.placeholder = "text";
+input.height = "506px";
+
+footer.appendChild(input);
+
+document.querySelectorAll("input").forEach((metin) => {
+  metin.addEventListener("focus", (event) => {
+    event.target.style.background = "orange";
+  });
+});
